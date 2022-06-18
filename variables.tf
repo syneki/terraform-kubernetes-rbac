419ef4d215866a18a80164e8aee55e17f5801409
Martin PAUCOT <m.paucot@kmp.agency>
2022-06-18 15:24:58 +0200

    feat: Add description to variables

--- variables.tf
+++ variables.tf
@@ -1,26 +1,33 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "Name used to create resources"
 }
 
 variable "namespace" {
-  type    = string
-  default = "default"
+  type        = string
+  default     = "default"
+  description = "Namespace where resources will be created"
 }
 
 variable "labels" {
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
+  description = "Labels append to created resources"
 }
 
 variable "cluster_role" {
   type = object({
     rules = set(map(list(string)))
   })
+
+  description = "Configuration for the Cluster Role"
 }
 
 variable "roles" {
   type = list(object({
-    name   = string
+    name  = string
     rules = set(map(string))
   }))
+
+  description = "Configuration for the Roles"
 }
